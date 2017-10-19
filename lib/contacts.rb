6467312a6f1deb7723d3c2cc577d982cb5391431
Julianne Kim <juliannne@gmail.com>
2017-10-19 18:16:27 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,10 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contacts_details_hash.each do |attribute, data|
-      binding.pry 
+      binding.pry
     end 
   end
-    # data.each do |attribute, value|
-    #   if value == "strawberry"
-
 end
